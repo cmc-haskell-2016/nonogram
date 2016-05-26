c536322277aed8120678a693f1c1736fc0a77422
Daniel Puchkin <danila.puchkin@mail.ru>
2016-05-27 01:12:20 +0300

    added Miner

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,13 @@
 --https://habrahabr.ru/post/249803/
 --Программы коллег на git'е
 
+--Actions:
+--sudo apt-get update
+--sido apt-get install ghci
+--sudo apt-get install stack
+--sudo apt-get install git
+--действия с git'a
+
 
 module Main where
 
