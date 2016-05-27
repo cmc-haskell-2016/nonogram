764ffacbb09614e20b79fa351f1aa2d83e885918
Daniel Puchkin <danila.puchkin@mail.ru>
2016-05-27 03:09:25 +0300

    added all actions to start 'hello world'

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,11 @@
 --Программы коллег на git'е
 
 --EasyGit:
---
+--обновление репозитория
 --git add <каталог или файл, который хотел бы добавить>
 --git commit -m "<комментарий к тому что добавил>"
 --git push
---
+--создание ветки
 --git branch <имя создаваемой ветки>
 --git branch -v ;список созданных веток
 --git checkout <имя ветки, к которой переходим>
@@ -20,6 +20,11 @@
 --sido apt-get install ghci
 --sudo apt-get install stack
 --sudo apt-get install git
+--sudo apt-get install cabal-install
+--sudo apt-get install freeglut3 freeglut3-dev
+--sudo apt-get install llvm
+--cabal update
+--cabal install gloss-examples
 --действия с git'a
 
 module Main where
