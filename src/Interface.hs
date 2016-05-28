3124202d5976b6e8a5c8ad930db092c7b0215122
Daniel Puchkin <danila.puchkin@mail.ru>
2016-05-28 23:44:25 +0300

    added renderer

--- src/Interface.hs
+++ src/Interface.hs
@@ -7,57 +7,74 @@ maxLen :: Task -> Int
 maxLen = maximum . map length
 
 drawNonogram :: Nonogram -> Picture
-drawNonogram Nonogram { field = board
-                      , cols  = top
+drawNonogram Nonogram { cols  = top
                       , rows  = left
-                      } = pictures[ drawFieldAt startPoint cellSize cellSize numCols numRows
-                      --            , drawCols  startPoint top
-                      --            , drawRows  startPoint left
-                                  ]
+                      } = pictures [ drawGrid startPoint cellSize cellSize (makeTopGrid top)
+                                   , drawGrid startPoint cellSize cellSize (makeBotGrid left)
+                                   , drawGrid startPoint cellSize cellSize emptyGrid
+                                   ]
   where
-    numRows = length board
-    numCols = length (head board)
     windowWidth = 500
     windowHeight = 500
     cellSize = 20
-    lenLeft = cellSize * (maxLen left)
-    lenTop = cellSize * (maxLen top)
+    lenLeft = cellSize * (maxLen left) + (div cellSize 2)
+    lenTop = cellSize * (maxLen top) - (div cellSize 2)
     startPoint = ( (div ((-1) * windowWidth) 2) + lenLeft
                  , (div windowHeight 2) - lenTop
                  )
+    emptyGrid = zip (repeat 0) [(x, y) | x <- [0 .. ((length top) - 1)]
+                                       , y <- [(negate (length left)) .. -1]
+                                       ]
 
-drawFieldAt :: Coord -> Int -> Int -> Int -> Int -> Picture
-drawFieldAt (i, j) width height c r = pictures [ translate (fromIntegral i) (fromIntegral j) 
-                                               $ scale (fromIntegral width) (fromIntegral height) 
-                                               $ drawField c r
-                                               ]
-
-drawField :: Int -> Int -> Picture
-drawField i j = pictures (map drawCell numCR)
+makeBotGrid :: Task -> [(Int, Coord)]
+makeBotGrid side = zip (foldr (++) [] side) (makeBotAccordance numXY)
   where
-    numRow = [x | x <- [(negate j) .. (negate 1)]]
-    numCol = [x | x <- [0 .. (i - 1)]]
-    numCR = [(x, y) | x <- numCol, y <- numRow]
+    numX  = [[(negate x) .. (negate 1)] | x <- map length side]
+    numXY = zip numX (reverse [(negate (length numX)) .. (negate 1)])
 
---drawCols :: Coord -> Task -> Picture
---drawCols _ = id
+makeBotAccordance :: [([Int], Int)] -> [Coord]
+makeBotAccordance = foldr (++) [] . map (\x -> zip (fst x) (repeat (snd x)))
 
---drawRows :: Coord -> Task -> Picture
---drawRows _ = id
-
-drawCell :: Coord -> Picture
-drawCell (i, j) = pictures [ translate (fromIntegral i) (fromIntegral j) $ color white $ rectangleSolid size size
-                           , translate (fromIntegral i) (fromIntegral j) $ color black $ rectangleWire size size
-                           ]
---  | a == E = color white $ rectangleWire size size
---  | a == D = color black $ rectangleSolid size size
---  | a == U = pictures [ drawCell E
---                      , line[(0, 0), (size, size)]
---                      , line[(0, size), (size, 0)]
---                      ]
---  | otherwise = error "Invalid field by 'drawCell'"
+makeTopGrid :: Task -> [(Int, Coord)]
+makeTopGrid side = zip (foldr (++) [] side) (makeTopAccordance numXY)
   where
-    size = 1
+    numY  = [[0 .. (x - 1)] | x <- map length side]
+    numXY = zip [0 .. ((length numY) - 1)] numY
+
+makeTopAccordance :: [(Int, [Int])] -> [Coord]
+makeTopAccordance = foldr (++) [] . map (\x -> zip (repeat (fst x)) (reverse (snd x)))
+
+drawGrid :: Coord -> Int -> Int -> [(Int, Coord)] -> Picture
+drawGrid (i, j) width height = translate (fromIntegral i) (fromIntegral j) 
+                             . scale (fromIntegral width) (fromIntegral height) 
+                             . pictures 
+                             . map drawCell
+
+
+drawCell :: (Int, Coord) -> Picture
+drawCell (n, (i, j))
+  | (n == 0)  = pictures [ translate x y 
+                         $ color white 
+                         $ rectangleSolid size size
+                         , translate x y 
+                         $ color black 
+                         $ rectangleWire size size
+                         ]
+  | otherwise = pictures [ translate x y 
+                         $ color white 
+                         $ rectangleSolid size size
+                         , translate x y
+                         $ color black 
+                         $ rectangleWire size size
+                         , translate (x - 0.25) (y - 0.25) 
+                         $ scale 0.005 0.005
+                         $ color black 
+                         $ text (show x)
+                         ]
+  where 
+    size = 1;
+       x = fromIntegral i;
+       y = fromIntegral j
 
 
 handleNonogram :: Event -> Nonogram -> Nonogram
